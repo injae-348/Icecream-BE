83da9d3438231c98d3a6b7da0c92b012c639e068
injae-348 <ant0593@naver.com>
2024-01-30 16:32:06 +0900

    Feat: create BankAccount, Expenditure, Income Serializer

--- noteproject/expenditure/admin.py
+++ noteproject/expenditure/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import BankAccount, Expenditure, Income
 
-# Register your models here.
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ('userId','name', 'balance')
+    search_fields = ('name', 'userId__username')
+
+@admin.register(Expenditure)
+class ExpenditureAdmin(admin.ModelAdmin):
+    list_display = ('date', 'tag', 'price', 'content', 'contain', 'bankAccountId')
+    search_fields = ('tag', 'content', 'bankAccountId__name')  # 'bankAccountId'는 ForeignKey로 연결된 BankAccount 모델의 name을 검색할 수 있도록 추가
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('date', 'tag', 'price', 'content', 'contain', 'bankAccountId')
+    search_fields = ('tag', 'content', 'bankAccountId__name')  # 'bankAccountId'는 ForeignKey로 연결된 BankAccount 모델의 name을 검색할 수 있도록 추가
+    
---
+++ noteproject/expenditure/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import BankAccount, Expenditure, Income
+
+class BankAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankAccount
+        fields = ['id','userId','name','balance']
+
+class ExpenditureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expenditure
+        fields = ['id','date','tag','price','content','contain']
+
+class IncomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Income
+        fields = ['id','date','tag','price','content','contain']
