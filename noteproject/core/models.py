ab0cdf674a7b534f6576f35c48d9f7a6565ec9ce
injae-348 <ant0593@naver.com>
2024-01-30 12:43:02 +0900

    feat(model): add BankAccountModel use AbstractModel & CostModel

--- noteproject/core/models.py
+++ noteproject/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    """
+    생성 시간과 수정 시간을 자동으로 업데이트해 주는 추상화 기반 클래스 모델
+    """
+    createdDate = models.DateTimeField(auto_now_add=True)
+    modifiedDate = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True     # 추상화 기초 클래스로 선언 => migration시 테이블 생성X
+
+
+class CostModel(TimeStampedModel):
+
+    date = models.DateField()
+    tag = models.CharField(max_length=30)
+    price = models.IntegerField()
+    content = models.CharField(max_length=100)
+    contain = models.BooleanField(verbose_name="계산시 포함 여부")
+    
+    def __str__(self):
+        return self.content
+    
+    class Meta:
+        ordering = ['-date']
--- noteproject/expenditure/models.py
+++ noteproject/expenditure/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import TimeStampedModel,CostModel
 
-# Create your models here.
+# 어디 계좌인지 혹은 현금인지 name에 작성
+class BankAccount(TimeStampedModel):
+    userId = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=63)
+    balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Expenditure(CostModel):
+    bankAccountId = models.ForeignKey(BankAccount,on_delete=models.CASCADE)
+
+
+class Income(CostModel):    
+    bankAccountId = models.ForeignKey(BankAccount,on_delete=models.CASCADE)
